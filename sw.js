68c131eb9f84df76d81948de2699406c3cf2320d
radio12dancestation <radio12webstation@gmail.com>
2025-04-20 09:08:33 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,15 +1,7 @@
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open('radio12').then(function(cache) {
-      return cache.addAll(['./', './index.html', './logo.png']);
-    })
-  );
+self.addEventListener('install', function (e) {
+  console.log('Service Worker installato');
 });
 
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
+self.addEventListener('fetch', function (event) {
+  // gestione fetch
 });
